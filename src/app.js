7e34bc08e1bb353f2bd450a3e3e142b37ab865dc
John Chang <johnchang9094@gmail.com>
2017-04-19 23:15:22 +0100

    Added <Featured /> to render when running app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Featured } from './components';
 
 class App extends Component {
     render() {
         return (
             <div>
-                JCPlayer
+                <Featured />
             </div>
         );
     }
