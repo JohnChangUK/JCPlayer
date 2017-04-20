7bbf58ad53d01fb6ce34bcae1e36465098602028
John Chang <johnchang9094@gmail.com>
2017-04-20 22:56:44 +0100

    Installed xml2js, now parsing xml to JSON format, and able to res.send(response.text) to the output

--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var superagent = require('superagent');
+var xml2js = require('xml2js');
 
 router.get('/', function(req, res, next) {
 
@@ -24,6 +25,20 @@ router.get('/', function(req, res, next) {
 
         return;
     }
+    
+    var xml = response.text;
+
+    xml2js.parseString(xml, function(err, result) {
+        var rss = result.rss;
+        var channel = rss.channel;
+        if (channel.length > 0)
+            channel = channel[0];
+
+        res.json({
+            confirmation: 'success',
+            podcast: channel
+        });
+    });
 
     res.send(response.text);
 
